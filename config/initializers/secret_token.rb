f4cb48522b83b479d3e0688083612f5ec82454f7
Hossam Hammady <github@hammady.net>
2013-06-29 12:55:30 +0300

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qursync::Application.config.secret_token = '724551d9bd7535b01d9cc1fa1f16331fd81f3edd439dd5d9efb1c3e1cb71b6d345d6050c85ba059b3bd350ec0f4925ea5f74c5041a3aaa858eeedc1e75a5fbf4'

29a1fde774f18cada161260ba864ef0a9047a22a
Hossam Hammady <github@hammady.net>
2014-05-31 21:39:21 +0300

    fixing color validator bug

--- app/models/color_validator.rb
+++ app/models/color_validator.rb
@@ -1,6 +1,6 @@
 class ColorValidator < ActiveModel::Validator
   def validate(record)
-    unless record.color.match(/\A#[0-9A-Fa-f]{6}\z/) || record.color.blank?
+    unless record.color.blank? || record.color.match(/\A#[0-9A-Fa-f]{6}\z/)
       record.errors[:color] << "if set, must be in RGB hex form: #XXXXXX, where X is a hex digit from 0 to f/F"
     end
   end
